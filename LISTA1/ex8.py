d6eaa0e19e849e0abc2d2ea7990048baeaf80704
Gabriel F <empresagabriel@gmail.com>
2019-09-15 13:15:21 -0300

    Fixing ex.8 of #1 list. It was incomplete before.

--- LISTA1/ex8.py
+++ LISTA1/ex8.py
@@ -1,4 +1,15 @@
-#  8. Dado um número inteiro positivo N, calcule e imprima o maior quadrado menor ou igual a N. Exemplo: N = 38, o maior quadrado que é menor ou igual a 38 é 36, imprimir 36.
+#  8. Dado um número inteiro positivo N, calcule e imprima o maior
+# quadrado menor ou igual a N. Exemplo: N = 38, o maior quadrado que é menor ou
+# igual a 38 é 36, imprimir 36.
 
 limite = int(input("Digite o N: "))
 
+menor_numero_satisfatorio = 1
+
+for numero in range(0, limite + 1):
+    quadrado = numero * numero
+
+    if quadrado <= limite:
+        menor_numero_satisfatorio = numero
+
+print(menor_numero_satisfatorio)
