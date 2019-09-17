241cf601a05bb83a6713371fc999d1cfe21f1d27
Gabriel F <empresagabriel@gmail.com>
2019-09-16 21:13:25 -0300

    Adding fourth exercise of the second list that I solved with Eduardo
    There's probably exists some other solution that I discovered while
    resolving this exercise with Claudio. I may commit that solution as a
    separate branch in some point in the future.

--- LISTA3/ex4.py
+++ LISTA3/ex4.py
@@ -1,16 +1,28 @@
-
-string = input("Digite um texto:")
+#Exercício 04 da lista 3
+string = (input("Insira uma sequência de caracteres terminada por ponto(.)"))
+char = ""
+contador = 0
 nova_string = ""
+espacos_vistos = 0
 
-acabei_de_ver_espaco = False
-for caractere in string:
-	if caractere == ' ':
-		if acabei_de_ver_espaco == False:
-			nova_string = nova_string + ' '
-		else:
-			pass
-	elif caractere != ' ':
-		nova_string  = nova_string + caractere
-	acabei_de_ver_espaco = (caractere == ' ')
+#Estruturando a lógica
+for character in string: #Para cada caractere na string
+    # Acabamos de ver o último espaço em uma sequência de espaços
+    if character != " " and char == " ": 
+        if espacos_vistos > 1:
+            nova_string = nova_string + " "
 
-print(nova_string)
+    if character != " ": # Não é espaço, ignore o caractere
+        nova_string = nova_string + character
+        espacos_vistos = 0
+
+    # É espaço, fique atento para ver se estamos em uma sequencia de espaços
+    if character == " ":
+        espacos_vistos = espacos_vistos + 1
+
+    char = character
+    contador = contador + 1
+
+if char == ".": #Indica o final da string dada pelo usuário
+    print(contador) #Mostra o número de caracteres
+    print(nova_string)
