daddb6b2dbc6ff7f980d6c834e6085494ce6d32a
Gabriel Francischini <empresagabriel@gmail.com>
2019-09-17 00:40:01 -0300

    Adding the fourth exercise of the 3rd list
    I solved this one with Eduardo and Claudio, using WingPython IDE

--- LISTA3/ex4.py
+++ LISTA3/ex4.py
@@ -1,28 +1,15 @@
-#Exercício 04 da lista 3
-string = (input("Insira uma sequência de caracteres terminada por ponto(.)"))
-char = ""
-contador = 0
-nova_string = ""
-espacos_vistos = 0
 
-#Estruturando a lógica
-for character in string: #Para cada caractere na string
-    # Acabamos de ver o último espaço em uma sequência de espaços
-    if character != " " and char == " ": 
-        if espacos_vistos > 1:
-            nova_string = nova_string + " "
+string = input("Digite um texto:")
+nova_string = str()
 
-    if character != " ": # Não é espaço, ignore o caractere
-        nova_string = nova_string + character
-        espacos_vistos = 0
+sequencia_de_espacos = 0
+for caractere in string:
+	if caractere == ' ':
+		sequencia_de_espacos = sequencia_de_espacos + 1
+	elif caractere != ' ':
+		if sequencia_de_espacos > 1:
+			nova_string = nova_string + ' '
+		nova_string  = nova_string + caractere
+		sequencia_de_espacos = 0
 
-    # É espaço, fique atento para ver se estamos em uma sequencia de espaços
-    if character == " ":
-        espacos_vistos = espacos_vistos + 1
-
-    char = character
-    contador = contador + 1
-
-if char == ".": #Indica o final da string dada pelo usuário
-    print(contador) #Mostra o número de caracteres
-    print(nova_string)
+print(nova_string)
